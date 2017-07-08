ad7464abaa23058def2f8c22eb49be24453d9650
MCMrARM <mcmrarm@gmail.com>
2017-07-08 12:33:49 +0200

    Android Studio update, make MessageFormatSettingsActivity use the default appbar

--- app/src/main/java/io/mrarm/irc/MessageFormatSettingsActivity.java
+++ app/src/main/java/io/mrarm/irc/MessageFormatSettingsActivity.java
@@ -37,8 +37,6 @@ public class MessageFormatSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_format_settings);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         MessageBuilder builder = MessageBuilder.getInstance(this);
 
