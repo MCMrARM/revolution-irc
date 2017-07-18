888b730c3549bec0096cf121cf3d549242265515
MCMrARM <mcmrarm@gmail.com>
2017-07-18 20:34:02 +0200

    Support away in WHOIS

--- app/src/main/java/io/mrarm/irc/UserBottomSheetDialog.java
+++ app/src/main/java/io/mrarm/irc/UserBottomSheetDialog.java
@@ -21,13 +21,14 @@ import io.mrarm.irc.util.StatusBarColorBottomSheetDialog;
 public class UserBottomSheetDialog {
 
     private Context mContext;
-    private BottomSheetDialog mDialog;
+    private StatusBarColorBottomSheetDialog mDialog;
     private RecyclerView mRecyclerView;
     private ItemAdapter mAdapter;
 
     private String mNick;
     private String mUser;
     private String mRealName;
+    private boolean mAway;
     private List<Pair<String, String>> mEntries = new ArrayList<>();
 
     public UserBottomSheetDialog(Context context) {
@@ -35,42 +36,51 @@ public class UserBottomSheetDialog {
     }
 
     public void requestData(String nick, ChatApi connection) {
-        setUser(nick, null, null);
+        setUser(nick, null, null, false);
         connection.sendWhois(nick, (WhoisInfo info) -> {
             mRecyclerView.post(() -> {
-                setUser(info.getNick(), info.getUser(), info.getRealName());
-                addEntry(R.string.user_hostname, info.getHost());
-                if (info.getServer() != null)
-                    addEntry(R.string.user_server, mContext.getString(R.string.user_server_format, info.getServer(), info.getServerInfo()));
-                if (info.getChannels() != null) {
-                    StringBuilder b = new StringBuilder();
-                    for (WhoisInfo.ChannelWithNickPrefixes channel : info.getChannels()) {
-                        if (b.length() > 0)
-                            b.append(mContext.getString(R.string.text_comma));
-                        if (channel.getPrefixes() != null)
-                            b.append(channel.getPrefixes());
-                        b.append(channel.getChannel());
-                    }
-                    addEntry(R.string.user_channels, b.toString());
-                }
-                if (info.getIdleSeconds() > 0)
-                    addEntry(R.string.user_idle, mContext.getResources().getQuantityString(R.plurals.time_seconds, info.getIdleSeconds(), info.getIdleSeconds()));
-                if (info.getLoggedInAsAccount() != null)
-                    addEntry(R.string.user_account, info.getLoggedInAsAccount());
-                if (info.isOperator())
-                    addEntry(R.string.user_server_op, mContext.getString(R.string.user_server_op_desc));
-                if (info.isConnectionSecure())
-                    addEntry(R.string.user_secure, mContext.getString(R.string.user_secure_desc));
+                setData(info);
             });
         }, null);
     }
 
-    public void setUser(String nick, String user, String realName) {
+    public void setData(WhoisInfo info) {
+        mEntries.clear();
+        setUser(info.getNick(), info.getUser(), info.getRealName(), (info.getAwayMessage() != null));
+        if (info.getAwayMessage() != null)
+            addEntry(R.string.user_away, info.getAwayMessage());
+        addEntry(R.string.user_hostname, info.getHost());
+        if (info.getServer() != null)
+            addEntry(R.string.user_server, mContext.getString(R.string.user_server_format, info.getServer(), info.getServerInfo()));
+        if (info.getChannels() != null) {
+            StringBuilder b = new StringBuilder();
+            for (WhoisInfo.ChannelWithNickPrefixes channel : info.getChannels()) {
+                if (b.length() > 0)
+                    b.append(mContext.getString(R.string.text_comma));
+                if (channel.getPrefixes() != null)
+                    b.append(channel.getPrefixes());
+                b.append(channel.getChannel());
+            }
+            addEntry(R.string.user_channels, b.toString());
+        }
+        if (info.getIdleSeconds() > 0)
+            addEntry(R.string.user_idle, mContext.getResources().getQuantityString(R.plurals.time_seconds, info.getIdleSeconds(), info.getIdleSeconds()));
+        if (info.getLoggedInAsAccount() != null)
+            addEntry(R.string.user_account, info.getLoggedInAsAccount());
+        if (info.isOperator())
+            addEntry(R.string.user_server_op, mContext.getString(R.string.user_server_op_desc));
+        if (info.isConnectionSecure())
+            addEntry(R.string.user_secure, mContext.getString(R.string.user_secure_desc));
+    }
+
+    public void setUser(String nick, String user, String realName, boolean away) {
         mNick = nick;
         mUser = user;
         mRealName = realName;
+        mAway = away;
         if (mAdapter != null)
             mAdapter.notifyDataSetChanged();
+        updateDialogStatusBarColor();
     }
 
     public void addEntry(int titleId, String value) {
@@ -98,6 +108,16 @@ public class UserBottomSheetDialog {
                 mRecyclerView.setMinimumHeight(bottom-top);
             }
         });
+        updateDialogStatusBarColor();
+    }
+
+    private void updateDialogStatusBarColor() {
+        if (mDialog == null)
+            return;
+        if (mAway)
+            mDialog.setStatusBarColor(mContext.getResources().getColor(R.color.userAwayColorPrimaryDark));
+        else
+            mDialog.setStatusBarColor(mContext.getResources().getColor(R.color.colorPrimaryDark));
     }
 
     public void show() {
@@ -145,19 +165,27 @@ public class UserBottomSheetDialog {
         }
 
         private class HeaderHolder extends RecyclerView.ViewHolder {
+            private View mContainer;
             private TextView mName;
             private TextView mNick;
             private TextView mUser;
 
             public HeaderHolder(View itemView) {
                 super(itemView);
+                mContainer = itemView;
                 mName = (TextView) itemView.findViewById(R.id.name);
                 mNick = (TextView) itemView.findViewById(R.id.nick);
                 mUser = (TextView) itemView.findViewById(R.id.user);
             }
 
             public void bind() {
-                mName.setText(UserBottomSheetDialog.this.mRealName);
+                if (mAway) {
+                    mName.setText(mContext.getString(R.string.user_title_away, mRealName));
+                    mContainer.setBackgroundResource(R.color.userAwayColorPrimary);
+                } else {
+                    mName.setText(UserBottomSheetDialog.this.mRealName);
+                    mContainer.setBackgroundResource(R.color.colorPrimary);
+                }
                 mNick.setText(UserBottomSheetDialog.this.mNick);
                 mUser.setText(UserBottomSheetDialog.this.mUser);
             }
