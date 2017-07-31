68291a8bf47f44abc8e4abda40d94cd7735c621b
MCMrARM <mcmrarm@gmail.com>
2017-07-31 13:33:10 +0200

    Set manageSpaceActivity in AndroidManifest

---
+++ app/src/main/java/io/mrarm/irc/StorageSettingsActivity.java
@@ -0,0 +1,37 @@
+package io.mrarm.irc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+
+public class StorageSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_list_with_fab);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        findViewById(R.id.add).setVisibility(View.GONE);
+
+        RecyclerView recyclerView = findViewById(R.id.items);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        StorageSettingsAdapter adapter = new StorageSettingsAdapter(this);
+        recyclerView.setItemAnimator(null);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
