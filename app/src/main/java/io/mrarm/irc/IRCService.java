8c459a5f7e66ffabb3c767122bb6c4e24833487f
MCMrARM <mcmrarm@gmail.com>
2017-08-17 22:34:54 +0200

    Fixed the idle notification on pre-5.0

--- app/src/main/java/io/mrarm/irc/IRCService.java
+++ app/src/main/java/io/mrarm/irc/IRCService.java
@@ -8,9 +8,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import io.mrarm.chatlib.dto.MessageInfo;
@@ -22,6 +23,8 @@ public class IRCService extends Service implements ServerConnectionManager.Conne
 
     public static final int IDLE_NOTIFICATION_ID = 100;
     public static final String ACTION_START_FOREGROUND = "start_foreground";
+
+    private static final String IDLE_NOTIFICATION_CHANNEL = "Idle Notification";
 
     private ConnectivityChangeReceiver mConnectivityReceiver = new ConnectivityChangeReceiver();
 
@@ -67,14 +70,16 @@ public class IRCService extends Service implements ServerConnectionManager.Conne
         if (action.equals(ACTION_START_FOREGROUND)) {
             Intent mainIntent = MainActivity.getLaunchIntent(this, null, null);
             int connectionCount = ServerConnectionManager.getInstance(this).getConnections().size();
-            Notification notification = new NotificationCompat.Builder(this)
+            NotificationCompat.Builder notification = new NotificationCompat.Builder(this, IDLE_NOTIFICATION_CHANNEL)
                     .setContentTitle(getString(R.string.service_title))
                     .setContentText(getResources().getQuantityString(R.plurals.service_status, connectionCount, connectionCount))
-                    .setSmallIcon(R.drawable.ic_server_connected)
                     .setPriority(NotificationCompat.PRIORITY_MIN)
-                    .setContentIntent(PendingIntent.getActivity(this, IDLE_NOTIFICATION_ID, mainIntent, PendingIntent.FLAG_CANCEL_CURRENT))
-                    .build();
-            startForeground(IDLE_NOTIFICATION_ID, notification);
+                    .setContentIntent(PendingIntent.getActivity(this, IDLE_NOTIFICATION_ID, mainIntent, PendingIntent.FLAG_CANCEL_CURRENT));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                notification.setSmallIcon(R.drawable.ic_server_connected);
+            else
+                notification.setSmallIcon(R.drawable.ic_notification_connected);
+            startForeground(IDLE_NOTIFICATION_ID, notification.build());
         }
         return START_STICKY;
     }
