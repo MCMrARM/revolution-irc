55a7acfd6bfc08946cf5703ecf477393ae98d5aa
MCMrARM <mcmrarm@gmail.com>
2017-08-24 21:46:20 +0200

    Theme colored buttons (storage)

---
+++ app/src/main/java/io/mrarm/irc/view/theme/ThemedButton.java
@@ -0,0 +1,28 @@
+package io.mrarm.irc.view.theme;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class ThemedButton extends Button {
+
+    public ThemedButton(Context context) {
+        super(context);
+    }
+
+    public ThemedButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        install(this, attrs);
+    }
+
+    public ThemedButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        install(this, attrs);
+    }
+
+    public static void install(Button button, AttributeSet attrs) {
+        ThemedView.setupBackground(button, attrs);
+    }
+
+}
---
+++ app/src/main/java/io/mrarm/irc/view/theme/ThemedView.java
@@ -0,0 +1,44 @@
+package io.mrarm.irc.view.theme;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import io.mrarm.irc.R;
+import io.mrarm.irc.util.StyledAttributesHelper;
+import io.mrarm.irc.util.ThemeHelper;
+
+public class ThemedView {
+
+    private static final int[] ATTRS_BG = { android.R.attr.background };
+
+    public static void setupBackground(View view, AttributeSet attrs) {
+        StyledAttributesHelper r = StyledAttributesHelper.obtainStyledAttributes(view.getContext(), attrs, ATTRS_BG);
+        int bgResId = r.getResourceId(android.R.attr.background, 0);
+        Log.e("Test", "T" + bgResId + " " + R.attr.colorAccent);
+        if (r.getResourceId(android.R.attr.background, 0) == R.color.colorPrimary)
+            view.setBackgroundColor(ThemeHelper.getPrimaryColor(view.getContext()));
+        else if (r.getResourceId(android.R.attr.background, 0) == R.color.colorAccent)
+            view.setBackgroundColor(ThemeHelper.getAccentColor(view.getContext()));
+        else if (r.getResourceId(android.R.attr.background, 0) == R.drawable.colored_button) {
+            ViewCompat.setBackgroundTintList(view, createColoredButtonColorStateList(view.getContext()));
+        }
+        r.recycle();
+    }
+
+    private static ColorStateList createColoredButtonColorStateList(Context ctx) {
+        int accentColor = ThemeHelper.getAccentColor(ctx);
+        int disabledColor = StyledAttributesHelper.getColor(ctx, R.attr.colorButtonNormal, 0);
+        return new ColorStateList(new int[][] {
+                new int[] { -android.R.attr.state_enabled },
+                new int[] { }
+        }, new int[] {
+                disabledColor,
+                accentColor
+        });
+    }
+
+}
