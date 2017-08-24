9e0051ea2b8829bc9c8c6b5755912a1ed0b086b5
MCMrARM <mcmrarm@gmail.com>
2017-08-24 22:58:56 +0200

    ThemedTextView: check the text appearance in addition to text color

--- app/src/main/java/io/mrarm/irc/view/theme/ThemedTextView.java
+++ app/src/main/java/io/mrarm/irc/view/theme/ThemedTextView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 import io.mrarm.irc.R;
@@ -12,7 +13,7 @@ import io.mrarm.irc.util.ThemeHelper;
 
 public class ThemedTextView extends AppCompatTextView {
 
-    private static final int[] ATTRS_COLOR = { android.R.attr.textColor };
+    private static final int[] ATTRS_COLOR = { android.R.attr.textColor, android.R.attr.textAppearance };
 
     public ThemedTextView(Context context) {
         super(context);
@@ -37,6 +38,15 @@ public class ThemedTextView extends AppCompatTextView {
     public static void setupTextColor(TextView view, AttributeSet attrs) {
         StyledAttributesHelper r = StyledAttributesHelper.obtainStyledAttributes(view.getContext(), attrs, ATTRS_COLOR);
         int colorResId = r.getResourceId(android.R.attr.textColor, 0);
+        if (colorResId == 0) {
+            int appearanceRes = r.getResourceId(android.R.attr.textAppearance, 0);
+            if (appearanceRes != 0) {
+                StyledAttributesHelper ta = StyledAttributesHelper.obtainStyledAttributes(
+                        view.getContext(), appearanceRes, new int[] { android.R.attr.textColor });
+                colorResId = ta.getResourceId(android.R.attr.textColor, 0);
+                ta.recycle();
+            }
+        }
         if (colorResId == R.color.colorPrimary)
             view.setTextColor(ThemeHelper.getPrimaryColor(view.getContext()));
         else if (colorResId == R.color.colorAccent)
