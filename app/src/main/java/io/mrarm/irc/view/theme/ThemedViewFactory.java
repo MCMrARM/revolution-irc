7e337fe3868e885897bdea5f1c4726dd79717799
MCMrARM <mcmrarm@gmail.com>
2017-08-25 20:42:21 +0200

    Theme SeekBar

--- app/src/main/java/io/mrarm/irc/view/theme/ThemedViewFactory.java
+++ app/src/main/java/io/mrarm/irc/view/theme/ThemedViewFactory.java
@@ -26,6 +26,8 @@ public class ThemedViewFactory implements LayoutInflater.Factory2 {
             return new ThemedSpinner(context, attrs);
         if (name.equals("TextView"))
             return new ThemedTextView(context, attrs);
+        if (name.equals("SeekBar"))
+            return new ThemedSeekBar(context, attrs);
         if (name.equals("android.support.design.widget.TextInputLayout"))
             return new ThemedTextInputLayout(context, attrs);
         if (name.equals("android.support.design.widget.TextInputEditText"))
