b4b3ce439925f458ac15c45c35d767bec1a81bf1
MCMrARM <mcmrarm@gmail.com>
2017-10-02 16:43:50 +0200

    Fixed CheckBox themed ripple color

--- app/src/main/java/io/mrarm/irc/view/theme/ThemedCheckBox.java
+++ app/src/main/java/io/mrarm/irc/view/theme/ThemedCheckBox.java
@@ -2,7 +2,12 @@ package io.mrarm.irc.view.theme;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.RippleDrawable;
+import android.os.Build;
 import android.support.v4.graphics.ColorUtils;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.CompoundButtonCompat;
 import android.support.v7.widget.AppCompatCheckBox;
 import android.util.AttributeSet;
@@ -34,6 +39,10 @@ public class ThemedCheckBox extends AppCompatCheckBox {
         if (ThemeHelper.hasCustomAccentColor(checkBox.getContext())) {
             CompoundButtonCompat.setButtonTintList(checkBox,
                     createCheckBoxTintStateList(checkBox.getContext()));
+            if (Build.VERSION.SDK_INT >= 21 && checkBox.getBackground() instanceof RippleDrawable) {
+                ((RippleDrawable) checkBox.getBackground()).setColor(
+                        createCheckBoxRippleTintStateList(checkBox.getContext()));
+            }
         }
     }
 
@@ -49,6 +58,19 @@ public class ThemedCheckBox extends AppCompatCheckBox {
         }, new int[] {
                 disabledColor,
                 accentColor,
+                normalColor
+        });
+    }
+
+    static ColorStateList createCheckBoxRippleTintStateList(Context ctx) {
+        int accentColor = ThemeHelper.getAccentColor(ctx);
+        int normalColor = StyledAttributesHelper.getColor(ctx, R.attr.colorControlHighlight, 0);
+        int accentAlphaColor = ColorUtils.setAlphaComponent(accentColor, (int) (255.f * 0.26f)); // highlight_alpha_material_colored
+        return new ColorStateList(new int[][] {
+                new int[] { android.R.attr.state_enabled, android.R.attr.state_checked },
+                new int[] { }
+        }, new int[] {
+                accentAlphaColor,
                 normalColor
         });
     }
