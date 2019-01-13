621e38e071853f9f886d029b5589e4475947da52
MCMrARM <mcmrarm@gmail.com>
2019-01-13 22:26:58 +0100

    ColorPicker/ColorHuePicker: handle touch

--- app/src/main/java/io/mrarm/irc/view/ColorHuePicker.java
+++ app/src/main/java/io/mrarm/irc/view/ColorHuePicker.java
@@ -6,9 +6,15 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ColorHuePicker extends View {
 
@@ -17,6 +23,14 @@ public class ColorHuePicker extends View {
     private float[] mTmpHSV = new float[3];
     private Rect mBmpRect = new Rect();
     private Rect mTmpRect = new Rect();
+    private RectF mTmpRectF = new RectF();
+    private float mCurrentValue = 0.f;
+
+    private Paint mHandlePaint;
+    private Paint mHandleInnerPaint;
+    private float mHandleHeight;
+
+    private List<HueChangeListener> mListeners = new ArrayList<>();
 
     public ColorHuePicker(Context context) {
         this(context, null);
@@ -29,10 +43,44 @@ public class ColorHuePicker extends View {
     public ColorHuePicker(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mPaint = new Paint();
+        mHandlePaint = new Paint();
+        mHandlePaint.setColor(0xFF000000);
+        mHandlePaint.setStyle(Paint.Style.STROKE);
+        mHandlePaint.setStrokeWidth(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                3.f, getResources().getDisplayMetrics()));
+        mHandleInnerPaint = new Paint();
+        mHandleHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                8.f, getResources().getDisplayMetrics());
+    }
+
+    public void addHueChangeListener(HueChangeListener listener) {
+        mListeners.add(listener);
+    }
+
+    public void removeHueChangeListener(HueChangeListener listener) {
+        mListeners.remove(listener);
+    }
+
+    public float getHueValue() {
+        return mCurrentValue;
+    }
+
+    public int getColorValue() {
+        mTmpHSV[0] = mCurrentValue;
+        mTmpHSV[1] = 1.f;
+        mTmpHSV[2] = 1.f;
+        return Color.HSVToColor(mTmpHSV);
+    }
+
+    public void setHueValue(float val) {
+        mCurrentValue = val;
+        invalidate();
+        for (HueChangeListener listener : mListeners)
+            listener.onHueChanged(val);
     }
 
     private void createBitmap() {
-        int h = getHeight();
+        int h = getHeight() - getPaddingTop() - getPaddingBottom();
         mBitmap = Bitmap.createBitmap(1, h, Bitmap.Config.ARGB_8888);
         mTmpHSV[1] = 1.f;
         mTmpHSV[2] = 1.f;
@@ -44,11 +92,40 @@ public class ColorHuePicker extends View {
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN ||
+                event.getAction() == MotionEvent.ACTION_MOVE ||
+                event.getAction() == MotionEvent.ACTION_UP) {
+            float val = (event.getY() - getPaddingTop()) /
+                    (getHeight() - getPaddingTop() - getPaddingBottom()) * 360.f;
+            val = Math.min(Math.max(val, 0.f), 360.f);
+            setHueValue(val);
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (mBitmap == null || mBitmap.getHeight() != getHeight())
+        int h = getHeight() - getPaddingTop() - getPaddingBottom();
+        if (mBitmap == null || mBitmap.getHeight() != h)
             createBitmap();
-        mTmpRect.set(0, 0, getWidth(), getHeight());
+        mTmpRect.set(getPaddingLeft(), getPaddingTop(),
+                getWidth() - getPaddingRight(), getHeight() - getPaddingBottom());
         canvas.drawBitmap(mBitmap, mBmpRect, mTmpRect, mPaint);
+        float handleY = getPaddingTop() + h * mCurrentValue / 360.f;
+        mTmpRectF.set(getPaddingLeft(), handleY - mHandleHeight / 2.f,
+                getWidth() - getPaddingRight(), handleY + mHandleHeight / 2.f);
+        mHandleInnerPaint.setColor(getColorValue());
+        canvas.drawRect(mTmpRectF, mHandleInnerPaint);
+        canvas.drawRect(mTmpRectF, mHandlePaint);
     }
+
+    public interface HueChangeListener {
+
+        void onHueChanged(float newHue);
+
+    }
+
 }
--- app/src/main/java/io/mrarm/irc/view/ColorPicker.java
+++ app/src/main/java/io/mrarm/irc/view/ColorPicker.java
@@ -2,22 +2,32 @@ package io.mrarm.irc.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class ColorPicker extends View {
 
-    private int mCurrentHue = 0xFFFF0000;
+    private float mCurrentHue = 0.f;
+    private float mCurrentSaturation = 1.f;
+    private float mCurrentValue = 1.f;
+    private Paint mFillPaint;
     private Rect mTmpRect = new Rect();
     private LinearGradient mSaturationGradient;
     private Paint mSaturationGradientPaint;
     private LinearGradient mValueGradient;
     private Paint mValueGradientPaint;
+    private Paint mCirclePaint;
+    private Paint mCircleInnerPaint;
+    private float mCircleSize;
+    private float[] mTmpHSV = new float[3];
 
     public ColorPicker(Context context) {
         this(context, null);
@@ -30,34 +40,90 @@ public class ColorPicker extends View {
     public ColorPicker(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
+        mFillPaint = new Paint();
         mSaturationGradientPaint = new Paint();
         mSaturationGradientPaint.setDither(true);
         mValueGradientPaint = new Paint();
         mValueGradientPaint.setDither(true);
+        mCirclePaint = new Paint();
+        mCirclePaint.setStyle(Paint.Style.STROKE);
+        mCirclePaint.setStrokeWidth(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                3.f, getResources().getDisplayMetrics()));
+        mCircleInnerPaint = new Paint();
+        mCircleSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                8.f, getResources().getDisplayMetrics());
     }
 
-    public void setHue(int currentHue) {
+    public void attachToHuePicker(ColorHuePicker picker) {
+        setHue(picker.getHueValue());
+        picker.addHueChangeListener(this::setHue);
+    }
+
+    public void setHue(float currentHue) {
         mCurrentHue = currentHue;
+        invalidate();
+    }
+
+    public int getColor() {
+        mTmpHSV[0] = mCurrentHue;
+        mTmpHSV[1] = mCurrentSaturation;
+        mTmpHSV[2] = mCurrentValue;
+        return Color.HSVToColor(mTmpHSV);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        mSaturationGradient = new LinearGradient(0, 0, w, 0,
+        mSaturationGradient = new LinearGradient(getPaddingLeft(), 0,
+                w - getPaddingRight(), 0,
                 0xFFFFFFFF, 0x00FFFFFF, Shader.TileMode.CLAMP);
         mSaturationGradientPaint.setShader(mSaturationGradient);
-        mValueGradient = new LinearGradient(0, 0, 0, h,
+        mValueGradient = new LinearGradient(0, getPaddingTop(), 0,
+                h - getPaddingRight(),
                 0x00000000, 0xFF000000, Shader.TileMode.CLAMP);
         mValueGradientPaint.setShader(mValueGradient);
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN ||
+                event.getAction() == MotionEvent.ACTION_MOVE ||
+                event.getAction() == MotionEvent.ACTION_UP) {
+            mCurrentSaturation = (event.getX() - getPaddingLeft()) /
+                    (getWidth() - getPaddingLeft() - getPaddingRight());
+            mCurrentValue = 1.f - (event.getY() - getPaddingTop()) /
+                    (getHeight() - getPaddingTop() - getPaddingBottom());
+            mCurrentSaturation = Math.min(Math.max(mCurrentSaturation, 0.f), 1.f);
+            mCurrentValue = Math.min(Math.max(mCurrentValue, 0.f), 1.f);
+            invalidate();
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
-        canvas.drawColor(mCurrentHue);
-        mTmpRect.set(0, 0, getWidth(), getHeight());
+        mTmpHSV[0] = mCurrentHue;
+        mTmpHSV[1] = 1.f;
+        mTmpHSV[2] = 1.f;
+        mFillPaint.setColor(Color.HSVToColor(mTmpHSV));
+        mTmpRect.set(getPaddingLeft(), getPaddingTop(),
+                getWidth() - getPaddingRight(), getHeight() - getPaddingBottom());
+        canvas.drawRect(mTmpRect, mFillPaint);
         canvas.drawRect(mTmpRect, mSaturationGradientPaint);
         canvas.drawRect(mTmpRect, mValueGradientPaint);
+        int w = getWidth() - getPaddingLeft() - getPaddingRight();
+        int h = getHeight() - getPaddingTop() - getPaddingBottom();
+        int currentColor = getColor();
+        mCircleInnerPaint.setColor(currentColor);
+        if (Color.red(currentColor) < 128 && Color.green(currentColor) < 128 &&
+                Color.blue(currentColor) < 128)
+            mCirclePaint.setColor(0xFFFFFFFF);
+        else
+            mCirclePaint.setColor(0xFF000000);
+        canvas.drawCircle(getPaddingLeft() + w * mCurrentSaturation, getPaddingTop() + h * (1.f - mCurrentValue), mCircleSize, mCircleInnerPaint);
+        canvas.drawCircle(getPaddingLeft() + w * mCurrentSaturation, getPaddingTop() + h * (1.f - mCurrentValue), mCircleSize, mCirclePaint);
     }
 
 
