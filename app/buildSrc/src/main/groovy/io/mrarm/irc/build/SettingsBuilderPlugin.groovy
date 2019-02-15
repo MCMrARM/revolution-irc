141848edceef3678668e47b1160a4c42f68e089b
MCMrARM <mcmrarm@gmail.com>
2019-02-15 17:16:32 +0100

    Initial settings generation code

---
+++ app/buildSrc/src/main/groovy/io/mrarm/irc/build/SettingsBuilderPlugin.groovy
@@ -0,0 +1,23 @@
+package io.mrarm.irc.build
+
+import com.android.build.gradle.AppExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class SettingsBuilderPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        def android = (AppExtension) project.extensions.getByName("android")
+
+        def genDir = new File(project.getBuildDir(), "generated/source/settings")
+        def genTask = project.task("generateSettings")
+        genTask << {
+            SettingsBuilder.generateJavaFiles(project.file("settings.yml"), genDir)
+        }
+        android.applicationVariants.all { v ->
+            v.registerJavaGeneratingTask(genTask, genDir)
+        }
+    }
+
+}
