02f7f70577dc8857d0a848ba5f1cc85c62fa4894
MCMrARM <mcmrarm@gmail.com>
2019-02-15 19:25:13 +0100

    SettingsBuilder: add static members for names

--- app/buildSrc/src/main/java/io/mrarm/irc/build/SettingsBuilder.java
+++ app/buildSrc/src/main/java/io/mrarm/irc/build/SettingsBuilder.java
@@ -38,7 +38,7 @@ public class SettingsBuilder {
 
         String prefPrefix = (String) map.getOrDefault("pref_prefix", null);
         List<String> imports = (List<String>) map.get("import");
-        List<Map> settings = (List<Map>) map.get("settings");
+        List<Map> settingsMap = (List<Map>) map.get("settings");
         List<String> helpers = (List<String>) map.get("helpers");
 
         JavaWriter writer = new JavaWriter(new FileWriter(outputFile));
@@ -59,24 +59,21 @@ public class SettingsBuilder {
                 EnumSet.of(Modifier.PRIVATE, Modifier.STATIC));
         writer.emitStatement("return SettingsHelper.getContext()");
         writer.endMethod();
+        writer.emitEmptyLine();
 
-        for (Map s : settings) {
-            String spec = (String) s.get("spec");
-            int typeIof = spec.indexOf(' ');
-            int setIof = spec.indexOf('=', typeIof + 1);
-            String type = spec.substring(0, typeIof).trim();
-            String name = (setIof != -1 ? spec.substring(typeIof + 1, setIof)
-                    : spec.substring(typeIof + 1)).trim();
-            String defaultValue =  setIof != -1 ? spec.substring(setIof + 1).trim() : null;
-            String prefName = (String) s.get("pref");
-            if (prefName == null) {
-                prefName = camelToPascal(name);
-                if (prefPrefix != null)
-                    prefName = prefPrefix + "_" + prefName;
-            }
-            writeGetterFunc(writer, type, name, prefName, defaultValue, s);
-            if (s.containsKey("setter"))
-                writeSetterFunc(writer, type, name, prefName, s);
+        List<SettingInfo> settings = new ArrayList<>();
+        for (Map m : settingsMap)
+            settings.add(parseSetting(m, prefPrefix));
+        for (SettingInfo s : settings) {
+            writer.emitField("String", s.staticMemberName,
+                    EnumSet.of(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL),
+                    "\"" + s.prefName + "\"");
+        }
+        writer.emitEmptyLine();
+        for (SettingInfo s : settings) {
+            writeGetterFunc(writer, s);
+            if (s.properties.containsKey("setter"))
+                writeSetterFunc(writer, s);
         }
 
         if (helpers != null) {
@@ -110,35 +107,57 @@ public class SettingsBuilder {
         writer.close();
     }
 
-    private static void writeGetterFunc(JavaWriter writer, String type, String name,
-                                        String prefName, String defValue, Map properties)
-            throws IOException {
-        String getterName = (String) properties.get("getter");
+    public static SettingInfo parseSetting(Map s, String prefPrefix) {
+        SettingInfo ret = new SettingInfo();
+        ret.properties = s;
+        String spec = (String) s.get("spec");
+        int typeIof = spec.indexOf(' ');
+        int setIof = spec.indexOf('=', typeIof + 1);
+        ret.type = spec.substring(0, typeIof).trim();
+        ret.name = (setIof != -1 ? spec.substring(typeIof + 1, setIof)
+                : spec.substring(typeIof + 1)).trim();
+        ret.defaultValue =  setIof != -1 ? spec.substring(setIof + 1).trim() : null;
+        ret.prefName = (String) s.get("pref");
+        if (ret.prefName == null) {
+            ret.prefName = camelToPascal(ret.name);
+            if (prefPrefix != null)
+                ret.prefName = prefPrefix + "_" + ret.prefName;
+        }
+        ret.staticMemberName = "PREF_" + camelToPascal(ret.name).toUpperCase();
+        return ret;
+    }
+
+    private static void writeGetterFunc(JavaWriter writer, SettingInfo setting) throws IOException {
+        String getterName = (String) setting.properties.get("getter");
         if (getterName == null)
-            getterName = getGetterPrefix(type) + capitalize(name);
+            getterName = getGetterPrefix(setting.type) + capitalize(setting.name);
 
-        writer.beginMethod(type, getterName, EnumSet.of(Modifier.PUBLIC, Modifier.STATIC));
+        writer.beginMethod(setting.type, getterName, EnumSet.of(Modifier.PUBLIC, Modifier.STATIC));
 
-        switch (type) {
+        switch (setting.type) {
             case "boolean":
-                writeSimpleGetterFuncBody(writer, "getBoolean", prefName, defValue);
+                writeSimpleGetterFuncBody(writer, "getBoolean",
+                        setting.staticMemberName, setting.defaultValue);
                 break;
             case "int":
-                writeSimpleGetterFuncBody(writer, "getInt", prefName, defValue);
+                writeSimpleGetterFuncBody(writer, "getInt",
+                        setting.staticMemberName, setting.defaultValue);
                 break;
             case "String":
-                writeSimpleGetterFuncBody(writer, "getString", prefName, defValue);
+                writeSimpleGetterFuncBody(writer, "getString",
+                        setting.staticMemberName, setting.defaultValue);
                 break;
             case "String[]":
-                writeStringArrGetterFuncBody(writer, prefName, defValue, properties);
+                writeStringArrGetterFuncBody(writer, setting.staticMemberName,
+                        setting.defaultValue, setting.properties);
                 break;
             case "long":
                 writer.emitStatement(
-                        "return SettingsHelper.getLong(getPreferences(), \"%s\", %s)",
-                        prefName, defValue);
+                        "return SettingsHelper.getLong(getPreferences(), %s, %s)",
+                        setting.staticMemberName, setting.defaultValue);
                 break;
             default:
-                throw new IllegalArgumentException("Type not supported: " + type);
+                throw new IllegalArgumentException("Type not supported: " + setting.type);
         }
 
         writer.endMethod();
@@ -152,7 +171,7 @@ public class SettingsBuilder {
 
     private static void writeSimpleGetterFuncBody(JavaWriter writer, String type, String prefName,
                                                   String defValue) throws IOException {
-        writer.emitStatement("return getPreferences().%s(\"%s\", %s)",
+        writer.emitStatement("return getPreferences().%s(%s, %s)",
                 type, prefName, defValue);
     }
 
@@ -160,7 +179,7 @@ public class SettingsBuilder {
                                                      String defValue, Map properties)
             throws IOException {
         String separator = (String) properties.get("separator");
-        writer.emitStatement("String s = getPreferences().getString(\"%s\", null)",
+        writer.emitStatement("String s = getPreferences().getString(%s, null)",
                 prefName);
         writer.beginControlFlow("if (s == null || s.length() == 0)");
         writer.emitStatement("return %s", defValue);
@@ -168,33 +187,32 @@ public class SettingsBuilder {
         writer.emitStatement("return s.split(String.valueOf(%s))", separator);
     }
 
-    private static void writeSetterFunc(JavaWriter writer, String type, String name,
-                                        String prefName, Map properties)
+    private static void writeSetterFunc(JavaWriter writer, SettingInfo setting)
             throws IOException {
-        Object setterNameObj = properties.get("setter");
+        Object setterNameObj = setting.properties.get("setter");
         String setterName = setterNameObj instanceof String
-                ? (String) properties.get("setter") : null;
+                ? (String) setting.properties.get("setter") : null;
         if (setterName == null)
-            setterName = "set" + capitalize(name);
+            setterName = "set" + capitalize(setting.name);
 
         writer.beginMethod("void", setterName,
-                EnumSet.of(Modifier.PUBLIC, Modifier.STATIC), type, "value");
+                EnumSet.of(Modifier.PUBLIC, Modifier.STATIC), setting.type, "value");
 
-        switch (type) {
+        switch (setting.type) {
             case "boolean":
-                writeSimpleSetterFuncBody(writer, "putBoolean", prefName);
+                writeSimpleSetterFuncBody(writer, "putBoolean", setting.staticMemberName);
                 break;
             case "int":
-                writeSimpleSetterFuncBody(writer, "putInt", prefName);
+                writeSimpleSetterFuncBody(writer, "putInt", setting.staticMemberName);
                 break;
             case "String":
-                writeSimpleSetterFuncBody(writer, "putString", prefName);
+                writeSimpleSetterFuncBody(writer, "putString", setting.staticMemberName);
                 break;
             case "long":
-                writeSimpleSetterFuncBody(writer, "putLong", prefName);
+                writeSimpleSetterFuncBody(writer, "putLong", setting.staticMemberName);
                 break;
             default:
-                throw new IllegalArgumentException("Type not supported: " + type);
+                throw new IllegalArgumentException("Type not supported: " + setting.type);
         }
 
         writer.endMethod();
@@ -202,7 +220,7 @@ public class SettingsBuilder {
 
     private static void writeSimpleSetterFuncBody(JavaWriter writer, String type, String prefName)
             throws IOException {
-        writer.emitStatement("getPreferences().edit().%s(\"%s\", %s).apply()",
+        writer.emitStatement("getPreferences().edit().%s(%s, %s).apply()",
                 type, prefName, "value");
     }
 
@@ -221,6 +239,16 @@ public class SettingsBuilder {
             ret.append(c);
         }
         return ret.toString();
+    }
+
+
+    public static class SettingInfo {
+        public String name;
+        public String type;
+        public String defaultValue;
+        public String prefName;
+        public String staticMemberName;
+        public Map properties;
     }
 
 }
