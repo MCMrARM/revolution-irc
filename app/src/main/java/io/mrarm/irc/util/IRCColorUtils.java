734a0d49ab8cf8e3e71e4213845bbb6590b9d3b1
Martin D Kealey <martin@kurahaupo.gen.nz>
2022-10-01 15:34:40 +1000

    Support additional colour codes 16-98

--- app/src/main/java/io/mrarm/irc/util/IRCColorUtils.java
+++ app/src/main/java/io/mrarm/irc/util/IRCColorUtils.java
@@ -38,12 +38,38 @@ public class IRCColorUtils {
             R.styleable.IRCColors_colorOrange,
             R.styleable.IRCColors_colorYellow,
             R.styleable.IRCColors_colorLightGreen,
+
             R.styleable.IRCColors_colorCyan,
             R.styleable.IRCColors_colorLightCyan,
             R.styleable.IRCColors_colorLightBlue,
             R.styleable.IRCColors_colorPink,
             R.styleable.IRCColors_colorGray,
-            R.styleable.IRCColors_colorLightGray
+            R.styleable.IRCColors_colorLightGray,
+            0xff470000, 0xff472100, 0xff474700, 0xff324700,
+
+            0xff004700, 0xff00472c, 0xff004747, 0xff002747, 0xff000047,
+            0xff2e0047, 0xff470047, 0xff47002a, 0xff740000, 0xff743a00,
+
+            0xff747400, 0xff517400, 0xff007400, 0xff007449, 0xff007474,
+            0xff004074, 0xff000074, 0xff4b0074, 0xff740074, 0xff740045,
+
+            0xffb50000, 0xffb56300, 0xffb5b500, 0xff7db500, 0xff00b500,
+            0xff00b571, 0xff00b5b5, 0xff0063b5, 0xff0000b5, 0xff7500b5,
+
+            0xffb500b5, 0xffb5006b, 0xffff0000, 0xffff8c00, 0xffffff00,
+            0xffb2ff00, 0xff00ff00, 0xff00ffa0, 0xff00ffff, 0xff008cff,
+
+            0xff0000ff, 0xffa500ff, 0xffff00ff, 0xffff0098, 0xffff5959,
+            0xffffb459, 0xffffff71, 0xffcfff60, 0xff6fff6f, 0xff65ffc9,
+
+            0xff6dffff, 0xff59b4ff, 0xff5959ff, 0xffc459ff, 0xffff66ff,
+            0xffff59bc, 0xffff9c9c, 0xffffd39c, 0xffffff9c, 0xffe2ff9c,
+
+            0xff9cff9c, 0xff9cffdb, 0xff9cffff, 0xff9cd3ff, 0xff9c9cff,
+            0xffdc9cff, 0xffff9cff, 0xffff94d3, 0xff000000, 0xff131313,
+
+            0xff282828, 0xff363636, 0xff4d4d4d, 0xff656565, 0xff818181,
+            0xff9f9f9f, 0xffbcbcbc, 0xffe2e2e2, 0xffffffff
     };
 
     private static int[] NICK_COLORS = new int[] { 3, 4, 7, 8, 9, 10, 11, 12, 13 };
